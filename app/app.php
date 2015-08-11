b94ec59d8f93985f05aace29e1371007f8523afa
Jose Torres & william swanson <student@epicodus.com>
2015-08-11 10:10:24 -0700

    This commit adds a basic setup for app.php

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Anagram.php";
+
+  $app = new Silex\Application();
+
+  $app->register (new Silex\Provider|TwigServiceProvider(), array(
+    'twig.path'==>__DIRD__.'/../views'
+  ));
+
+  $app->get("/", function() use ($app) {
+    return $app['twig']-render(anagram_form.html.twig)
+  })
+
+
+return $app;
+ ?>
