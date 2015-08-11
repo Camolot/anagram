485194e32bd175c3b1602d86013559f87727e8d7
Jose Torres & william swanson <student@epicodus.com>
2015-08-11 11:49:11 -0700

    completed spec sheet, began writing AnagramGenerator.php

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -5,8 +5,25 @@
 
         function makeAnagramMatch($input_word)
         {
-            
+            $input_array_list = array();
+            $output_anagram = array();
+
+            if (strlen($input_array_list) > strlen($input_word) {
+                
+
+
+
+            })
         }
+
+
+
+
+
+
+
+
+
 
 
 
