36e7c73ef44ddd1cfb2b14525cf5f8ec09a2ef0c
Jose Torres & william swanson <student@epicodus.com>
2015-08-11 16:02:31 -0700

    This commit saves AnagramGenerator.php before replacing with a new approach to a solution.

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -8,7 +8,10 @@
 
             $output_list = array();
 
+            $input_list = implode(" ",$input_list);
+
             if ($input_list == $input_word) {
+                echo explode(" ",$input_list);
                 array_push($output_list, $input_list);
 
             }
