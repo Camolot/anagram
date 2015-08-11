f4d863ea6350aac77cafa578f139d6223d4a1d51
Jose Torres & william swanson <student@epicodus.com>
2015-08-11 15:29:15 -0700

    This commit includes improvements to AnagramGenerator.php

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -5,28 +5,16 @@
 
         function makeAnagramMatch($input_word, $input_list)
         {
-            $input_list = array();
-            $input_word = array();
+
             $output_list = array();
 
-            if ($input_list = $input_word) {
-                            }
+            if ($input_list == $input_word) {
+                array_push($output_list, $input_list);
 
+            }
+        return $output_list;
          }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
     ?>
--- tests/AnagramGeneratorTest.php
+++ tests/AnagramGeneratorTest.php
@@ -5,12 +5,20 @@
     class AnagramGeneratorTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_makeAnagram_singleLetter()
+        function test_makeAnagramMatch_singleLetter()
         {
 
 
             //Arrange
-            $test_
+            $test_makeAnagramMatch = new AnagramGenerator;
+            $input_word = "a";
+            $input_list = ["a"];
+
+            //Act
+            $result = $test_makeAnagramMatch->makeAnagramMatch($input_word, $input_list);
+
+            //Assert
+            $this->assertEquals(["a"], $result);
         }
     }
 
